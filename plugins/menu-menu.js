2057dc8930e0a3b422a9da63379a6d03f871943b
𝐒𝐘𝐒𝐓𝐄𝐌 𝐁𝐎𝐓 <benjaminexe2023@gmail.com>
2024-07-14 18:07:01 -1100

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   try {
     const pp = imagen1;
     // let vn = './media/menu.mp3'
-    const img = './src/MenuCompleto.jpeg';
+    const img = './Menu1.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
